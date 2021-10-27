2f478c9c15a865e0f3dd3385a8dc248bbf43d262
Juha Lauriakainen <jmlaurikainen@gmail.com>
2021-10-27 06:12:36 +0300

    Data formatting and header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,76 @@
 import faker from "faker";
-import React, { useMemo } from "react";
+import React, { Fragment, useMemo } from "react";
 import { CellProps, Column, useSortBy, useTable } from "react-table";
 
+interface DataFragment {
+  value: string;
+  sortValue?: number | string;
+}
+
 interface Data {
-  id: number;
-  name: { first: string; last: string };
-  address: string;
-  email: string;
+  id: DataFragment;
+  name: DataFragment;
+  address: DataFragment;
+  email: DataFragment;
 }
 
 const data: Data[] = new Array(10).fill(true).map((_, i) => ({
-  id: i,
-  name: {
-    first: faker.name.firstName(),
-    last: faker.name.lastName(),
+  id: {
+    value: i.toString(),
+    sortValue: i,
   },
-  address: faker.address.city(),
-  email: faker.internet.email(),
+  name: {
+    value: faker.name.findName(),
+    sortValue: Math.floor(Math.random() * 10),
+  },
+  address: {
+    value: faker.address.city(),
+    sortValue: Math.floor(Math.random() * 10),
+  },
+  email: {
+    value: faker.internet.email(),
+    sortValue: Math.floor(Math.random() * 10),
+  },
 }));
 
-const CustomCell = (props: CellProps<Data>) => {
-  const {
-    id,
-    name: { first, last },
-    address,
-    email,
-  }: Data = props.data[props.row.index];
+const HeaderCell = (props: CellProps<Data>) => {
+  return <Fragment>{props.column.id}</Fragment>;
+};
 
-  return <div>{`${last}, ${first}`}</div>;
+const CustomCell = (props: CellProps<Data>) => {
+  const key = props.cell.column.id as keyof Data;
+  const data = props.cell.row.original[key];
+
+  return <div>{data.value}</div>;
 };
 
 const App = () => {
   const columns: Column<Data>[] = useMemo(
     () => [
-      { Header: "ID", accessor: "id" },
       {
-        Header: "Name",
-        accessor: (data) => data.name.last,
+        id: "id",
+        accessor: (data) => data.id.value,
+        Header: HeaderCell,
         Cell: CustomCell,
       },
-      { Header: "Address", accessor: "address" },
-      { Header: "Email", accessor: "email" },
+      {
+        id: "name",
+        accessor: (data) => data.name.value,
+        Header: HeaderCell,
+        Cell: CustomCell,
+      },
+      {
+        id: "address",
+        accessor: (data) => data.address.value,
+        Header: HeaderCell,
+        Cell: CustomCell,
+      },
+      {
+        id: "email",
+        accessor: (data) => data.email.value,
+        Header: HeaderCell,
+        Cell: CustomCell,
+      },
     ],
     []
   );
@@ -60,7 +90,7 @@ const App = () => {
                 return (
                   <th {...column.getHeaderProps(column.getSortByToggleProps())}>
                     {column.render("Header")}
-                    {column.isSorted ? (column.isSortedDesc ? "D" : "U") : ""}
+                    {column.isSorted ? (column.isSortedDesc ? " D" : " U") : ""}
                   </th>
                 );
               })}
